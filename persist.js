1a7bd65cf655607fe5c7fad687b6cd6a9fa4ee84
Clemens Damke <admin@cortys.de>
2015-12-25 00:16:30 +0100

    various minor bugfixes and cleanups

--- persist.js
+++ persist.js
@@ -1,7 +1,10 @@
 "use strict";
 
 module.exports = function persist(object) {
+	if(!("$loki" in object && "meta" in object))
+		return Promise.resolve();
+
 	return object.constructor.store.collection
-		? object.constructor.store.collection.update(object)
+		? Promise.resolve(object.constructor.store.collection.update(object))
 		: object.constructor.store.loaded.then(() => persist(object));
 };
