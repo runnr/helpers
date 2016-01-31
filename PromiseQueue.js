03299b469c5d071ac81ff4c6b84aa15366760c83
Clemens Damke <admin@cortys.de>
2016-01-31 18:07:35 +0100

    Runner#activate now waits if its runner is disabled (rejected before)
    Implementing this feature caused a chain reaction across five modules,
    to make it work. The following steps were taken:
    
    1. This causes runners to be only activated once at startup. Every
    runner would be started, immediately shut down and then started again
    before, because they were activated on Runner#assign, deactivated by
    the auto update routine of Plugin#assign (if they depended on
    autoupdating plugins) and then activated again by the Plugin update
    routine.
    
    Now Runner#activate will delay activation until this runner is enabled.
    
    2. To make 1 possible: By default all runners are disabled at startup
    now and will be enabled if Runner#graph.loaded resolves.
    
    3. To make 2 possible: Added Graph#loaded promise, that gets resolved
    if all Node#loaded promises of its nodes are resolved.
    
    4. To make 3 possible: Added Node#loaded promise, that gets resolved if
    a Node is ready to be used by a Runner runtime. DataNode#loaded
    resolved immediately. PluginNode#loaded resolves after
    PluginNode#plugin.loaded resolves.
    
    5. To make 4 possible: Added Plugin#loaded that gets resolved after
    Plugin got assigned and (if applicable) its initial autoupdate was
    completed.

--- PromiseQueue.js
+++ PromiseQueue.js
@@ -1,16 +1,46 @@
 "use strict";
 
+const onEmpty = Symbol("onEmpty");
+
 class PromiseQueue extends Set {
+	constructor() {
+		super();
+
+		this[onEmpty] = {
+			promise: Promise.resolve(true)
+		};
+	}
+
 	add(promise) {
 		promise = Promise.resolve(promise);
 
 		const remove = () => {
 			this.delete(promise);
+
+			if(this.isEmpty && this[onEmpty].resolve)
+				this[onEmpty].resolve();
 		};
 
 		promise.then(remove, remove);
 
+		if(!this[onEmpty].resolve) {
+			this[onEmpty].promise = new Promise(resolve => {
+				this[onEmpty].resolve = () => {
+					this[onEmpty].resolve = undefined;
+					resolve(true);
+				}
+			});
+		}
+
 		return super.add(promise);
+	}
+
+	get isEmpty() {
+		return this.size === 0;
+	}
+
+	get onEmpty() {
+		return this[onEmpty].promise;
 	}
 }
 
