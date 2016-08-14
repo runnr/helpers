6425708eadbd275d661d375be9faf8033dc752fa
Clemens Damke <admin@cortys.de>
2016-08-15 00:34:21 +0200

    added delayedUnlock to lock

--- lock.js
+++ lock.js
@@ -1,10 +1,13 @@
 "use strict";
 
-module.exports = function generateLock() {
+module.exports = function generateLock(delayedUnlock) {
 	let unlock;
 	const lock = new Promise(resolve => unlock = resolve);
 
 	lock.unlock = unlock;
+
+	if(typeof delayedUnlock === "function")
+		setImmediate(() => unlock(delayedUnlock()));
 
 	return lock;
 };
