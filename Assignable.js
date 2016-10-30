8934aec268dd38d6ea0600bf11ccc8dcce944177
Clemens Damke <admin@cortys.de>
2016-10-30 23:44:53 +0100

    added Assignable mixin
    An Assignable offers an "assign()" method and a "loaded" getter that returns a Promise that resolves as soon as no assign call is unsettled and at least one assign call has been settled.

---
+++ Assignable.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const { Mixin } = require("mixwith");
+
+const PromiseQueue = require("./PromiseQueue");
+const generateLock = require("./generateLock");
+
+const loaded = Symbol("loaded");
+const assignLock = Symbol("assignLock");
+
+const Assignable = Mixin(superclass => class Assignable extends superclass {
+	constructor() {
+		super(...arguments);
+
+		this[loaded] = new PromiseQueue();
+		this[assignLock] = generateLock();
+
+		this[loaded].add(this[assignLock]);
+	}
+
+	assign(promise) {
+		this[loaded].add(promise);
+		this[assignLock].unlock();
+
+		return promise;
+	}
+
+	get loaded() {
+		return this[loaded].onEmpty;
+	}
+});
+
+module.exports = Assignable;
