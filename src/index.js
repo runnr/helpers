9e7810d5f2aed1cc16afc2784b4dacb289769d12
Clemens Damke <admin@cortys.de>
2016-12-14 23:56:23 +0100

    added package metadata

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+	Assignable: require("./Assignable"),
+	filterObject: require("./filterObject"),
+	generateLock: require("./generateLock"),
+	generatingMaps: require("./generatingMaps"),
+	internalize: require("./internalize"),
+	PromiseQueue: require("./PromiseQueue")
+};
